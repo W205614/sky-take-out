75401906be60b04ea4af26bb9202d8e2decc2964
W205614 <2545284366@qq.com>
2025-12-27 23:03:02 +0800

    取消订单业务开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -76,4 +76,17 @@ public class OrderController {
         OrderVO orderVO = orderService.details(id);
         return Result.success(orderVO);
     }
+
+    /**
+     * 取消订单
+     * @param id
+     * @return
+     */
+    @PutMapping("/cancel/{id}")
+    @ApiOperation("取消订单")
+    public Result cancel(@PathVariable("id") Long id) throws Exception{
+        log.info("取消订单, 订单id为: {}", id);
+        orderService.userCancelById(id);
+        return Result.success();
+    }
 }
