056f07674f50f2dbe1401f53b844185f722c39a5
W205614 <2545284366@qq.com>
2025-12-28 23:53:34 +0800

    客户催单业务开发

--- sky-server/src/main/java/com/sky/task/WebSocketTask.java
+++ sky-server/src/main/java/com/sky/task/WebSocketTask.java
@@ -13,9 +13,9 @@ public class WebSocketTask {
     private WebSocketServer webSocketServer;
 
     /**
-     * 通过WebSocket每隔5秒向客户端发送消息
+     * 通过WebSocket每隔10秒向客户端发送消息
      */
-    @Scheduled(cron = "0/10 * * * * ?")
+    // @Scheduled(cron = "0/10 * * * * ?")
     public void sendMessageToClient() {
         webSocketServer.sendToAllClient("这是来自服务端的消息：" + DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
     }
